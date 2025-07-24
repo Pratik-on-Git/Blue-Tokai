dcf52f42dc5c8d4727b6dfe5a6415eaae758dc5a
Pratik Das <pratikdasmartingarrix@gmail.com>
2025-07-24 05:55:24 +0530

    debug

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -64,6 +64,17 @@ const Loader = ({ onFinish }) => {
     return () => document.removeEventListener("click", restart);
   }, [onFinish]);
 
+  useEffect(() => {
+    if (visible) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, [visible]);
+
   if (!visible) return null;
 
   return (
